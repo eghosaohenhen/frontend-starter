02ef677abcd31381611e4ca4426cd3af2964d2fa
Eghosa Ohenhen <eohenhen@mit.edu>
2023-10-28 20:27:55 -0400

    no error again

--- client/views/LoginView.vue
+++ client/views/LoginView.vue
@@ -5,8 +5,32 @@ import RegisterForm from "@/components/Login/RegisterForm.vue";
 
 <template>
   <main class="column">
-    <h1>Please login or register!</h1>
-    <LoginForm />
-    <RegisterForm />
+    <img src="@/assets/images/picture.png" />
+    <h2>Please login or register!</h2>
+    <div class = "row">
+      <LoginForm />
+      <RegisterForm />
+
+    </div>
   </main>
 </template>
+<style>
+template,body{
+  background: linear-gradient(217deg, var(--light-pink), rgba(255,0,0,0) 70.71%),
+            linear-gradient(127deg, var(--light-yellow), rgba(0,255,0,0) 70.71%),
+            linear-gradient(336deg, var(--muted-lavender), rgba(0,0,255,0) 70.71%);
+}
+h2{
+  font-family: 'Starborn';
+  font-size: medium;
+}
+.row{
+  display:flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+img{
+  height: 250px;
+  padding: 50px;
+}
+</style>
